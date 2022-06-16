e158a98b4dabde93079545ae3214b2a2ee38c77c
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-15 23:15:45 -0500

    added logic

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const apiKey = '425415860de8eaa385a38bf9b34c848f';
+
+    const cityEl = $('h2#city');
+    const dateEl = $('h3#date');
+    const weatherIconEl = $('img#weather-icon');
+    const temperatureEl = $('span#temperature');
+    const humidityEl = $('span#humidity');
+    const windEl = $('span#wind');
+    const uvIndexEl = $('span#uv-index');
+    const cityListEl = $('div.cityList');
+
+   
+   const cityInput = $('#city-input');
+
+
+   let pastCities = [];
+}
+
+
+);
